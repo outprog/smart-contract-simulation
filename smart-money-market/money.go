6f90c41dbdbe7c0c940c73c7211a0446a9e50240
outprog <xiongwei_13@hotmail.com>
2019-01-29 15:30:48 +0800

    smart-money-market: supply & borrow

--- smart-money-market/money.go
+++ smart-money-market/money.go
@@ -1,16 +1,18 @@
 package money
 
 import (
-	"log"
+	"fmt"
 )
 
 type tUser = string
 type tSymbol = string
 type balance = float64
 
+// Market is token ledger
 type Market struct {
 	supply float64
 	borrow float64
+	growth float64
 }
 
 type MoneyContract struct {
@@ -33,7 +35,15 @@ func New() *MoneyContract {
 	}
 }
 
-func (m *MoneyContract) Supply(amount float64, symbol string, user string) {
+func (m *MoneyContract) Supply(amount float64, symbol string, user string) error {
+	// check market
+	var market Market
+	var ok bool
+	if market, ok = m.Markets[symbol]; !ok {
+		return fmt.Errorf("not support token: %v", symbol)
+	}
+
+	// update user supply
 	if supply, ok := m.SupplyBalances[user]; ok {
 		if _, ok := supply[symbol]; ok {
 			m.SupplyBalances[user][symbol] += amount
@@ -44,22 +54,40 @@ func (m *MoneyContract) Supply(amount float64, symbol string, user string) {
 		m.SupplyBalances[user] = map[tSymbol]float64{symbol: amount}
 	}
 
-	m.Markets[symbol] = Market{
-		supply: m.Markets[symbol].supply + amount,
-		borrow: m.Markets[symbol].borrow,
-	}
+	// update market supply
+	market.supply += amount
+	m.Markets[symbol] = market
+
+	return nil
 }
 
-func (m *MoneyContract) Borrow(amount float64, symbol string, user string) {
+func (m *MoneyContract) Borrow(amount float64, symbol string, user string) error {
+	// check market
 	var market Market
 	var ok bool
 	if market, ok = m.Markets[symbol]; !ok {
-		log.Printf("not support token: %v", symbol)
-		return
+		return fmt.Errorf("not support token: %v", symbol)
 	}
 
+	// check cash
 	if cash := market.supply - market.borrow; cash < amount {
-		log.Printf("not enough cash: %v", cash)
-		return
+		return fmt.Errorf("not enough cash: %v", cash)
 	}
+
+	// update user borrow
+	if borrow, ok := m.BorrowBalances[user]; ok {
+		if _, ok := borrow[symbol]; ok {
+			m.BorrowBalances[user][symbol] += amount
+		} else {
+			borrow[symbol] = amount
+		}
+	} else {
+		m.BorrowBalances[user] = map[tSymbol]float64{symbol: amount}
+	}
+
+	// update market borrow
+	market.borrow += amount
+	m.Markets[symbol] = market
+
+	return nil
 }
--- smart-money-market/money_test.go
+++ smart-money-market/money_test.go
@@ -1,6 +1,7 @@
 package money
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,9 +9,16 @@ import (
 
 func TestSupply(t *testing.T) {
 	mc := New()
+
 	user := "Alice"
 	amount := 100.0
 	symbol := "ETH"
+
+	amount2 := 50.0
+	symbol2 := "DAI"
+
+	amount3 := 1.0
+	symbol3 := "TFT"
 
 	mc.Supply(amount, symbol, user)
 	assert.Equal(t, amount, mc.Markets[symbol].supply)
@@ -20,18 +28,40 @@ func TestSupply(t *testing.T) {
 	assert.Equal(t, amount*2, mc.Markets[symbol].supply)
 	assert.Equal(t, amount*2, mc.SupplyBalances[user][symbol])
 
-	amount2 := 50.0
-	symbol2 := "DAI"
 	mc.Supply(amount2, symbol2, user)
 	assert.Equal(t, amount2, mc.Markets[symbol2].supply)
 	assert.Equal(t, amount2, mc.SupplyBalances[user][symbol2])
+
+	err := mc.Supply(amount3, symbol3, user)
+	assert.EqualError(t, errors.New("not support token: TFT"), err.Error())
 }
 
 func TestBorrow(t *testing.T) {
 	mc := New()
 	user := "Alice"
+
 	amount := 100.0
 	symbol := "ETH"
-	mc.Borrow(amount, "TFT", user)
-	mc.Borrow(amount, symbol, user)
+
+	user2 := "Bob"
+	amount2 := 33.3
+
+	// borrow with err
+	err := mc.Borrow(amount, "TFT", user)
+	assert.EqualError(t, errors.New("not support token: TFT"), err.Error())
+	err = mc.Borrow(amount, symbol, user)
+	assert.EqualError(t, errors.New("not enough cash: 0"), err.Error())
+
+	// borrow success
+	mc.Supply(amount, symbol, user)
+	err = mc.Borrow(amount2, symbol, user2)
+	assert.NoError(t, err)
+	assert.Equal(t, amount2, mc.Markets[symbol].borrow)
+	assert.Equal(t, amount2, mc.BorrowBalances[user2][symbol])
+
+	// borrow again
+	err = mc.Borrow(amount2, symbol, user2)
+	assert.NoError(t, err)
+	assert.Equal(t, amount2*2, mc.Markets[symbol].borrow)
+	assert.Equal(t, amount2*2, mc.BorrowBalances[user2][symbol])
 }
