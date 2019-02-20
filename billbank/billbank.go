5f6d93971c899aa693f3a7b6a05aaf31dad9ee8e
outprog <xiongwei_13@hotmail.com>
2019-02-20 22:52:54 +0800

    impl borrow; impl liquidate

--- billbank/billbank.go
+++ billbank/billbank.go
@@ -1,6 +1,9 @@
 package billbank
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 type tSymbol = string
 
@@ -8,6 +11,14 @@ type TokenPool struct {
 	SupplyBill float64
 	Supply     float64
 	Borrow     float64
+
+	// last liquidate blockNumber
+	borrowIndex uint64
+}
+
+// GetCash Cash = Supply - Borrow
+func (t *TokenPool) GetCash() float64 {
+	return t.Supply - t.Borrow
 }
 
 type Billbank struct {
@@ -15,6 +26,8 @@ type Billbank struct {
 
 	// BlockNumber simulate
 	BlockNumber uint64
+	// borrowRate every block
+	borrowRate float64
 }
 
 func New() *Billbank {
@@ -24,16 +37,13 @@ func New() *Billbank {
 			"DAI": TokenPool{},
 		},
 		BlockNumber: 1,
+		borrowRate:  0.01,
 	}
 }
 
 func (b *Billbank) Deposit(amount float64, symbol string) error {
-	// check pool
-	var pool TokenPool
-	var ok bool
-	if pool, ok = b.Pools[symbol]; !ok {
-		return fmt.Errorf("not support token: %v", symbol)
-	}
+	b.liquidate(symbol)
+	pool := b.getPool(symbol)
 
 	// liquidate supply
 
@@ -53,19 +63,15 @@ func (b *Billbank) Deposit(amount float64, symbol string) error {
 }
 
 func (b *Billbank) Withdraw(bill float64, symbol string) error {
-	// check pool
-	var pool TokenPool
-	var ok bool
-	if pool, ok = b.Pools[symbol]; !ok {
-		return fmt.Errorf("not support token: %v", symbol)
-	}
+	b.liquidate(symbol)
+	pool := b.getPool(symbol)
 
 	// liquidate supply
 
 	// check balance of supply
 	amount := bill * (pool.Supply / pool.SupplyBill)
 	if bill > pool.SupplyBill || amount > pool.Supply {
-		return fmt.Errorf("not enough token: %v > %v", amount, pool.Supply)
+		return fmt.Errorf("not enough token for withdraw. amount: %v, supply %v", amount, pool.Supply)
 	}
 
 	// update pool
@@ -80,9 +86,44 @@ func (b *Billbank) Withdraw(bill float64, symbol string) error {
 }
 
 func (b *Billbank) Borrow(amount float64, symbol string) error {
+	b.liquidate(symbol)
+	pool := b.getPool(symbol)
+
+	// check cash of pool
+	if amount > pool.GetCash() {
+		return fmt.Errorf("not enough token for borrow. amount: %v, cash: %v", amount, pool.Supply-pool.Borrow)
+	}
+
+	// update borrow
+	pool.Borrow += amount
+	b.Pools[symbol] = pool
+
 	return nil
 }
 
 func (b *Billbank) Repay(amount float64, symbol string) error {
+	b.liquidate(symbol)
 	return nil
+}
+
+func (b *Billbank) liquidate(symbol string) {
+	pool := b.getPool(symbol)
+
+	receivable := 0.0
+	if pool.Borrow != 0.0 {
+		receivable = pool.Borrow * b.borrowRate * float64(b.BlockNumber-pool.borrowIndex)
+	}
+	pool.Supply += receivable
+
+	// update pool
+	pool.borrowIndex = b.BlockNumber
+	b.Pools[symbol] = pool
+}
+
+func (b *Billbank) getPool(symbol string) (pool TokenPool) {
+	var ok bool
+	if pool, ok = b.Pools[symbol]; !ok {
+		log.Panicf("not support token: %v", symbol)
+	}
+	return
 }
--- billbank/billbank_test.go
+++ billbank/billbank_test.go
@@ -31,11 +31,44 @@ func TestWithdraw(t *testing.T) {
 	assert.Equal(t, 100.0, b.Pools["ETH"].SupplyBill)
 
 	err := b.Withdraw(101.0, "ETH")
-	assert.EqualError(t, errors.New("not enough token: 101 > 100"), err.Error())
+	assert.EqualError(t, errors.New("not enough token for withdraw. amount: 101, supply 100"), err.Error())
 	assert.Equal(t, 100.0, b.Pools["ETH"].Supply)
 	assert.Equal(t, 100.0, b.Pools["ETH"].SupplyBill)
 
 	b.Withdraw(99.0, "ETH")
 	assert.Equal(t, 1.0, b.Pools["ETH"].Supply)
 	assert.Equal(t, 1.0, b.Pools["ETH"].SupplyBill)
+}
+
+func TestBorrow(t *testing.T) {
+	b := New()
+
+	err := b.Borrow(10.0, "ETH")
+	assert.EqualError(t, errors.New("not enough token for borrow. amount: 10, cash: 0"), err.Error())
+
+	b.Deposit(100.0, "ETH")
+
+	err = b.Borrow(10.0, "ETH")
+	assert.NoError(t, err)
+	assert.Equal(t, 10.0, b.Pools["ETH"].Borrow)
+}
+
+func TestLiquidate(t *testing.T) {
+	b := New()
+
+	b.BlockNumber = 1
+	b.Deposit(100.0, "ETH")
+	b.Borrow(10.0, "ETH")
+
+	b.BlockNumber = 2
+	b.Deposit(0.0, "ETH")
+	assert.Equal(t, 100.1, b.Pools["ETH"].Supply)
+
+	b.BlockNumber = 4
+	b.Borrow(10.0, "ETH")
+	assert.Equal(t, 100.3, b.Pools["ETH"].Supply)
+
+	b.BlockNumber = 10
+	b.Borrow(0.0, "ETH")
+	assert.Equal(t, 100.3+20*6*0.01, b.Pools["ETH"].Supply)
 }
